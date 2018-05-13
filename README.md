256bfb15c763506a3c314d61899e0e2f2ea9d213
root <root@Omega-333D.lan>
2018-05-13 12:19:34 +0000

    Add README and document bash script for coffeebot

---
+++ README.md
@@ -0,0 +1,3 @@
+# Onion Omega2 scripts and code
+
+Included are scripts used for running the coffee bean humidity sensor attached to the Onion Omega2.
--- coffeebot_data.sh
+++ coffeebot_data.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# Script for testing humitity sensor connected to pin 19 of onion-omega
+# Take a reading from the humidity sensor, add that reading to the data CSV file
+# Requires dht-sensor which can be installed with `opkg update && opkg install dht-sensor`
+
 while true;
 do
     dht-sensor 19 DHT11 json >> coffeebot_data.csv
